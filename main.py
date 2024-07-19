254a3dc4ffd5652633d34e6765f2d8b382f66541
Zawskillearning <143388118+Zawskillearning@users.noreply.github.com>
2024-07-19 21:31:50 +0630

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 
 def search_video(song_name):
     query = song_name.replace(' ', '+')
-    url = f"https://apis.deepjyoti30.dev/v2/ytmdl/search?q={query}"
+    url = f"http://yangtautauaja.xp3.biz/v1/jugger.php?text={query}"
     try:
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
         if data:
-            video_url = data[0]["url"]
+            video_url = data["url"]
             return video_url
         return None
     except requests.RequestException as e:
@@ -21,31 +21,12 @@ def search_video(song_name):
     except ValueError as e:
         print(f"Error decoding JSON: {e}")
         return None
-
-def extract_audio(video_url):
-    try:
-        yt = YouTube(video_url)
-        audio_stream = yt.streams.filter(only_audio=True).first()
-        if audio_stream:
-            return audio_stream.url
-        return None
-    except Exception as e:
-        print(f"Error extracting audio: {e}")
-        return None
 #Host it in Vercel.com
-@app.route('/apiurl', methods=['GET'])
+@app.route('/ai', methods=['GET'])
 def download_audio():
-    song_name = request.args.get('songname')
+    song_name = request.args.get('prompt')
     if not song_name:
         return jsonify({"error": "Please provide a song name"}), 400
-
-    video_url = search_video(song_name)
-    if not video_url:
-        return jsonify({"error": "No video found or video is blocked"}), 404
-
-    audio_url = extract_audio(video_url)
-    if not audio_url:
-        return jsonify({"error": "Audio extraction failed"}), 500
 
     return jsonify({"download_link": audio_url})
 
